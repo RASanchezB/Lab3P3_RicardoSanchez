812718a35ed8b8939b78e00ea77e4598e8abbea4
RASanchezB <rasanchez@unitec.edu>
2017-10-27 14:29:10 -0600

    Inicio ejercicio 2

--- Lab3P3_RS.cpp
+++ Lab3P3_RS.cpp
@@ -5,3 +5,53 @@
 using namespace std;
 
 
+int Ejercicio1();
+int Ejercicio2(int);
+int Ejercicio3();
+
+
+int main(){
+	int opcion = 0;
+	while(opcion != 4){
+		cout<<"Laboratorio 3 Programacion 3"<<endl;
+		cout<<"-----MENU-----"<<endl
+	    	<< "1) Podar y buscar"<<endl
+	    	<< "2) Teorema de Rufin"<<endl
+	    	<< "3) Fechas"<<endl
+	    	<< "4) Salir"<<endl
+	    	<< "Ingrese el numero del ejercicio que desea:" <<endl;
+		cin >> opcion;
+		switch(opcion){
+			case 1:{
+				cout<<"Ejercicio 1:"<<endl;
+				
+				}
+				break;
+			case 2:{
+				cout<<"Ejercicio 2:"<<endl;
+				cout<<"Ingrese el grado mas grande del polinomio"<<endl;
+				int GradoM;
+				cin >> GradoM;
+				Ejercicio2(GradoM);
+				}
+				break;
+			case 3:{
+				cout<<"Ejercicio 3:"<<endl;
+				
+				}
+				break;
+			Default:{}
+				break;
+		}
+	}
+}
+
+int Ejercicio2(int Grado){
+	vector <int> Ecuacion;
+	int temp;
+	for(int i = 0; i <= Grado; i++){
+		cout<<"Ingrese el numero con el grado xᶺ"<<endl;
+		cin >> temp;
+		
+	}
+}
